45b644f809cd44f82fd9a2197c04bd3162d874a2
SaiTingHu <987947865@qq.com>
2020-09-17 18:19:12 +0800

    新增：纹理绘画器。

---
+++ Editor/Painter/TexturePainter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HT.TextureProcessor
+{
+    /// <summary>
+    /// 纹理绘画器
+    /// </summary>
+    public sealed class TexturePainter : IDisposable
+    {
+        /// <summary>
+        /// 销毁
+        /// </summary>
+        public void Dispose()
+        {
+            
+        }
+    }
+}
